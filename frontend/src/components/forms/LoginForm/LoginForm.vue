915966e5b6d9d5074281144a4e86f94a452cc37d
itolog <paukanlove@gmail.com>
2025-05-20 20:29:33 +0300

    add form ui

--- frontend/src/components/forms/LoginForm/LoginForm.vue
+++ frontend/src/components/forms/LoginForm/LoginForm.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const email = ref('')
-const password = ref('')
-
-const submit = () => {
-  console.log(email.value, password.value)
-}
+const schema = ref({
+  email: { type: 'text', label: 'Email', rules: ['required', 'email'] },
+  password: { type: 'text', label: 'Password', rules: ['required'] },
+  button: { type: 'button', buttonLabel: 'Login', submits: true },
+})
 </script>
 
 <template>
@@ -18,42 +17,7 @@ const submit = () => {
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form novalidate class="flex flex-col gap-4" @submit.prevent="submit">
-        <div>
-          <label for="email" class="block text-sm/6 font-medium">Email address</label>
-          <div class="mt-2">
-            <input
-              v-model="email"
-              type="email"
-              name="email"
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium">Password</label>
-          </div>
-          <div class="mt-2">
-            <input
-              v-model="password"
-              type="password"
-              name="password"
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
+      <Vueform endpoint="http://localhost:3000/api/v1/auth/login" method="post" :schema="schema" />
     </div>
   </div>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+import Vueform from '@vueform/vueform'
+import vueformConfig from './../vueform.config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vueform, vueformConfig)
 
 app.mount('#app')
