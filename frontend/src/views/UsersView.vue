cce01a2f69114fdd22ea0bd828329a80f611043d
itolog <paukanlove@gmail.com>
2025-05-22 19:05:28 +0300

    add users | fetch data

--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
+import type { AxiosError, AxiosResponse } from "axios";
+import { watchEffect } from "vue";
+
+import { axios } from "@/configs/axiosConfig";
+import type { ApiResponseData, ApiResponseError } from "@/generated/apiClient/data-contracts";
+import type { User } from "@/types/user";
+
+const { isLoading, data } = useQuery<
+  AxiosResponse<ApiResponseData<User[]>>,
+  AxiosError<ApiResponseError>
+>({
+  queryKey: ["users"],
+  queryFn: async () => await axios.get("api/v1/user"),
+  retry: false,
+});
+
+watchEffect(() => {
+  console.log(data.value?.data.data);
+});
+</script>
 
 <template>
-  <div>USer</div>
+  <div>
+    <span v-if="isLoading">LOADING</span>
+    <div>USer</div>
+  </div>
 </template>
 
 <style scoped></style>
